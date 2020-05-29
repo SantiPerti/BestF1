2626f8f683d1b546aa1b5572bf86645e5ba7cba6
Santi Pérez <santiperez@gmail.com>
2020-05-29 21:17:20 +0200

    Initial Commit

---
+++ BestF1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BestF1
+//
+//  Created by Santiago Pérez Martínez on 29/05/2020.
+//  Copyright © 2020 Santiago Pérez Martínez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
